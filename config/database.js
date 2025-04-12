f08904fd6d7b85337e71cba0c305bb7ec4791738
Selvan <69357513+Selvan-DEV@users.noreply.github.com>
2025-04-12 22:45:21 +0530

    New Db added

--- config/database.js
+++ config/database.js
@@ -1,17 +1,18 @@
 const mysql = require('mysql2');
 
-const pool = mysql.createPool({
-  host: 'localhost',
-  user: 'root',
-  password: '',
-  database: 'e_commerce_db'
-});
-
 // const pool = mysql.createPool({
-//   host: 'beghjv350jzcqpcapmlc-mysql.services.clever-cloud.com',
-//   user: 'u6dumgcj4kmxwwmh',
-//   password: 'BIugeiLMmKTuhCP1HieK',
-//   database: 'beghjv350jzcqpcapmlc'
+//   host: 'localhost',
+//   user: 'root',
+//   password: '',
+//   database: 'e_commerce_db'
 // });
+
+const pool = mysql.createPool({
+  host: 'turntable.proxy.rlwy.net',
+  user: 'root',
+  password: 'fPUkYJoRrenwmSlNRxCztTxVJOeSUTVC',
+  database: 'e-commerce-db',
+  port: 10244
+});
 
 module.exports = pool.promise();
