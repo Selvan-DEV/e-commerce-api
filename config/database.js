bafacd6d4547e904be8070ecafef4776d96b40ea
Selvan <69357513+Selvan-DEV@users.noreply.github.com>
2025-05-19 17:19:13 +0530

    Data base config changed

--- config/database.js
+++ config/database.js
@@ -7,20 +7,20 @@ const mysql = require('mysql2');
 //   database: 'e_commerce_db'
 // });
 
-const pool = mysql.createPool({
-  host: 'turntable.proxy.rlwy.net',
-  user: 'root',
-  password: 'fPUkYJoRrenwmSlNRxCztTxVJOeSUTVC',
-  database: 'e-commerce-db',
-  port: 10244
-});
-
 // const pool = mysql.createPool({
-//   host: '89.116.21.79',
-//   user: 'selvan',
-//   password: 'Nilaash@2020',
-//   database: 'nilaa-foods-db',
-//   port: 3306
+//   host: 'turntable.proxy.rlwy.net',
+//   user: 'root',
+//   password: 'fPUkYJoRrenwmSlNRxCztTxVJOeSUTVC',
+//   database: 'e-commerce-db',
+//   port: 10244
 // });
+
+const pool = mysql.createPool({
+  host: '89.116.21.79',
+  user: 'root',
+  password: 'Nilaash@2020',
+  database: 'nilaa_foods_db',
+  port: 3306
+});
 
 module.exports = pool.promise();
